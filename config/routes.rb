d2b1432fe3fd58e42c0f237ff0371c24043ab41a
Davide Almeida <davide.jhonatan@gmail.com>
2022-07-27 17:48:17 -0300

    feat: config credentials and add google login btn
    Co-authored-by: Philipe Leandro <philipe-leandro@hotmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   devise_for :users, controllers: {
-    registrations: 'users/regsitrations',
+    registrations: 'users/registrations',
     sessions: 'users/sessions',
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
